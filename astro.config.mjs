c6b2f49860a258dd1c6cddea3668e2fc70abaf16
alexgilabert <alex04gilabert@gmail.com>
2025-10-04 18:29:44 +0200

    Configurar GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), react()] 
+  site: 'https://alexgilabert.github.io',
+  base: '/blanq-web',
+  integrations: [tailwind(), react()]
 });
